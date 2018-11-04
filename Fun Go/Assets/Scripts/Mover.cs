623a934ffbfd99de02d3f467cc49b86fd4c1ee2e
Amin Shazrin <aminshazrin@yahoo.com>
2018-11-04 16:55:29 +0800

    Done Move the car

---
+++ Fun Go/Assets/Scripts/Mover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+    private Rigidbody[] rb;
+    public Transform baseObject; // To move along with the objects
+    public float speed;
+    public float rotation;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponentsInChildren<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(x, 0.0f, z);
+
+        // Move for each components
+        foreach (Rigidbody child in rb)
+        {
+            child.velocity = movement * speed;
+        }
+        baseObject.position = this.gameObject.transform.position;
+    }
+}
