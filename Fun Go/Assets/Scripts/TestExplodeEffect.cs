55e501e380d60273bb8535f28a800e6d88d8cde5
Amin Shazrin <aminshazrin@yahoo.com>
2018-12-12 02:17:18 +0800

    Add new effect script for testing

---
+++ Fun Go/Assets/Scripts/TestExplodeEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestExplodeEffect : MonoBehaviour {
+
+    public GameObject remains;
+
+    // Source : https://gamedevelopment.tutsplus.com/tutorials/how-to-make-an-object-shatter-into-smaller-fragments-in-unity--gamedev-11795
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(remains, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
